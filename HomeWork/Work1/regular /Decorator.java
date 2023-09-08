8c132b1b9fe6de30913a11c503109b0edc05480c
Dmitry Legochev <114214910+gUmka07@users.noreply.github.com>
2023-09-08 13:10:00 +0700

    Delete HomeWork/Work1/regular /Decorator.java

--- HomeWork/Work1/regular /Decorator.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrains.lesson1.regular;
-
-
-public class Decorator {
-    public static String decorate (int a) {
-      
-        return String.format("Here is your number: %d.", a);
-    }
